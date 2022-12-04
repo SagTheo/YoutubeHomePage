4b921c25d24d7ec0de005b3b6074ccd24492ec56
Theo <sagniez.theo@laposte.net>
2022-12-04 13:01:41 +0100

    links block done in side bar

---
+++ src/linksSideBar.js
@@ -0,0 +1,20 @@
+const links1 = [
+    { title: 'About', link: 'https://youtube.com' },
+    { title: 'Press', link: 'https://youtube.com' },
+    { title: 'Copyright', link: 'https://youtube.com' },
+    { title: 'Contact us', link: 'https://youtube.com' },
+    { title: 'Creator', link: 'https://youtube.com' },
+    { title: 'Advertise', link: 'https://youtube.com' },
+    { title: 'Developers', link: 'https://youtube.com' },
+    { title: 'Report hateful content under LCEN', link: 'https://youtube.com' }
+]
+
+const links2 = [
+    { title: 'Terms', link: 'https://youtube.com' },
+    { title: 'Privacy', link: 'https://youtube.com' },
+    { title: 'Policy & safety', link: 'https://youtube.com' },
+    { title: 'How Youtube works', link: 'https://youtube.com' },
+    { title: 'Test new features', link: 'https://youtube.com' }
+]
+
+export const links = { links1, links2 }
