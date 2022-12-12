a402dc1ec4d5d45ccfe0fd9d08345d07db368dcd
Theo <sagniez.theo@laposte.net>
2022-12-12 21:02:25 +0100

    fixed arrow positionning + worked on its hover effect

--- src/components/SuggestionsBar.js
+++ src/components/SuggestionsBar.js
@@ -19,9 +19,11 @@ const SuggestionsBar = () => {
     <div className={styles.container} onScroll={showArrow} onMouseLeave={hideArrow}>
       <div className={styles.linksContainer}>
         <div className={`${styles.arrow} ${displayArrow ? styles.arrowShown : null}`}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-caret-left" viewBox="0 0 16 16">
-            <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
-          </svg>
+          <div className={styles.insideContainer}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-caret-left" viewBox="0 0 16 16">
+              <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+            </svg>
+          </div>
         </div>
         {
             suggestions.map(suggestion => {
