7ee282bd890cd4d571966bcd808da72af68ec8e4
Theo <sagniez.theo@laposte.net>
2022-12-14 20:52:22 +0100

    arrow hover effect fixed + implemented scroll on click of arrow

--- src/components/SuggestionsBar.js
+++ src/components/SuggestionsBar.js
@@ -15,15 +15,24 @@ const SuggestionsBar = () => {
     }
   }
 
+  const scroll = (e) => {
+    e.target.parentNode.parentNode.parentNode.scrollBy(-100, 0)
+  }
+
   return (
     <div className={styles.container} onScroll={showArrow} onMouseLeave={hideArrow}>
       <div className={styles.linksContainer}>
         <div className={`${styles.arrow} ${displayArrow ? styles.arrowShown : null}`}>
-          <div className={styles.insideContainer}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-caret-left" viewBox="0 0 16 16">
-              <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
-            </svg>
-          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" 
+                width="20" 
+                height="20" 
+                fill="white" 
+                className="bi bi-caret-left" 
+                viewBox="0 0 16 16"
+                onClick={scroll}
+          >
+            <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+          </svg>
         </div>
         {
             suggestions.map(suggestion => {
