2b753bc035a0e1df65ede6f3c7ec2dd2664509ef
Theo <sagniez.theo@laposte.net>
2023-01-02 19:42:12 +0100

    made suggestions bar fixed so doesn't scroll with youtube cards

--- src/youtubeCards/youtubeCards.js
+++ src/youtubeCards/youtubeCards.js
@@ -102,5 +102,109 @@ export const youtubeCards = [
             views: '137k',
             postedAt: '12 days ago'
         }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '8:00',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'How to one arm pull-up',
+        youtuber: 'FitnessFAQs',
+        videoStats: {
+            views: '342k',
+            postedAt: '1 year ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '12:00',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'Best of flips 2018',
+        youtuber: 'Dominik Sky',
+        videoStats: {
+            views: '450k',
+            postedAt: '4 years ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '23:16',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'A message to the truckers',
+        youtuber: 'Jordan B. Peterson',
+        videoStats: {
+            views: '1M',
+            postedAt: '10 months ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '8:34',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'Una sessione di allenamento con me',
+        youtuber: 'Andrea Larosa',
+        videoStats: {
+            views: '137k',
+            postedAt: '12 days ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '8:00',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'How to one arm pull-up',
+        youtuber: 'FitnessFAQs',
+        videoStats: {
+            views: '342k',
+            postedAt: '1 year ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '12:00',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'Best of flips 2018',
+        youtuber: 'Dominik Sky',
+        videoStats: {
+            views: '450k',
+            postedAt: '4 years ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '23:16',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'A message to the truckers',
+        youtuber: 'Jordan B. Peterson',
+        videoStats: {
+            views: '1M',
+            postedAt: '10 months ago'
+        }
+    },
+    {
+        thumbnailPic: 'https://picsum.photos/300/200', 
+        thumbnailAlt: 'test', 
+        videoDuration: '8:34',
+        profilePic: 'https://picsum.photos/40',
+        profileAlt: 'test',
+        title: 'Una sessione di allenamento con me',
+        youtuber: 'Andrea Larosa',
+        videoStats: {
+            views: '137k',
+            postedAt: '12 days ago'
+        }
     }
 ]
